c73d7d24ab66d8a7ee52340c1ce8c8e0cbe76f52
Tim <tim@spreadmaster.net>
2012-11-15 11:14:51 +0100

    init

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hanoi {
+    class Program {
+        static int countMoves = 0;
+        static void Main(string[] args) {
+            Begin:
+                char from = 'A', to = 'C', help = 'B';
+                int x = giveInt();
+                Console.WriteLine("\n  from = A, help = B, to = C\n");
+                hanoi(x, from, to, help);
+                Console.WriteLine("Moves: " + countMoves);
+                countMoves = 0;
+            goto Begin;
+        }
+
+        static void hanoi(int x, char from, char to, char help) {
+            if (x > 0) {
+                countMoves++;
+                x--;
+                hanoi(x, from, help, to);
+                move(x, from, to);
+                hanoi(x, help, to, from);
+            }
+
+        }
+
+        static void move(int x, char from, char to) {
+            Console.WriteLine("  move disk " + x + " from " + from + " to " + to);
+        }
+        static int giveInt() {
+            int x;
+            do {
+
+                try {
+                    Console.Write("  input number of disk: ");
+                    x = Int32.Parse(Console.ReadLine());
+                } catch (FormatException e) {
+                    x = -10;
+                }
+            } while (x < 1 || x > 15);
+            return x;
+        }
+
+    }
+}
